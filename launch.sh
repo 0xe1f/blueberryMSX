8e2aca43902bd2de62e1d24a73da3e5f5dbeea9f
Akop Karapetyan <dev@akop.org>
2025-12-24 02:10:51 -0800

    Launch dsk & cas

--- launch.sh
+++ launch.sh
@@ -5,6 +5,18 @@ cd `dirname $(readlink -f $0)`
 ROM=$1
 shift
 
+LAUNCH=""
+if [ -f roms/$ROM.rom ]; then
+    LAUNCH="-rom1 roms/$ROM.rom"
+elif [ -f roms/$ROM.dsk ]; then
+    LAUNCH="-diskA roms/$ROM.dsk"
+elif [ -f roms/$ROM.cas ]; then
+    LAUNCH="-cas roms/$ROM.cas"
+else
+    echo "No ROM or disk image found for $ROM" >&2
+    exit 1
+fi
+
 sleep 1
-echo "Launching TERM=vt220 ./bluemsx -rom1 roms/$ROM.rom $@" >> log.txt 2>&1
-TERM=vt220 ./bluemsx -rom1 roms/$ROM.rom $@ >> log.txt 2>&1
+echo "Launching TERM=vt220 ./bluemsx $LAUNCH $@" >> log.txt 2>&1
+TERM=vt220 ./bluemsx $LAUNCH $@ >> log.txt 2>&1
